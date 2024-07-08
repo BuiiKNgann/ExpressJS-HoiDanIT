087c3812efdc4aab96c8f3781935077e4d53199c
BuiiKNgann <buikimngan22130@gmail.com>
2024-07-08 21:38:52 +0700

    seven

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');// require express
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-
+const connection = require('./config/database')
 
 const app = express();// khởi tạo express
 const port = process.env.PORT || 8888;
@@ -16,7 +16,16 @@ configViewEngine(app);
 //Khai báo route
 app.use('/', webRoutes)
 
+//test connection
 
+//simple query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>>results= ", results);
+
+    }
+)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
