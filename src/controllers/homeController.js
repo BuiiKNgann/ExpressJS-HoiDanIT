5b396f51ff699f4704c97352d11387642c4b5a39
BuiiKNgann <buikimngan22130@gmail.com>
2024-07-09 14:01:30 +0700

    eight

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,21 @@
+const connection = require('../config/database');
+
 const getHomePage = (req, res) => {
-    res.send('Hello World! & nodemon')
+    //process data
+    //call model
+    let users = [];
+
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results= ", results);
+
+            // console.log('check users: ', users);
+            res.send(JSON.stringify(users))
+        }
+    );
+
 
 }
 
